75645ee80b7fff0bc68c10de87769aad009c7906
Jan <jstroemp@gmail.com>
2021-11-02 20:44:43 +0100

    added healthcheck function

--- crypto_api.py
+++ crypto_api.py
@@ -3,9 +3,9 @@ from flask_restful import Resource, Api, reqparse, abort
 
 from exchanges import Gemini, Kraken, Bitstamp
 
-bitstamp = Bitstamp("jany")
-gemini = Gemini("jany")
-kraken = Kraken("jany")
+bitstamp = Bitstamp("example")
+gemini = Gemini("example")
+kraken = Kraken("example")
 
 
 app = Flask(__name__)
@@ -23,9 +23,15 @@ def abort_if_invalid_exchange(exchange):
     if exchange not in defined_exchanges:
         abort(404, message="Exchange " + exchange + " not included in project. Use Gemini, Kraken or Bitstamp")
 
+class HealthCheck(Resource):
 
-class Balance(Resource):
-    def get(self, exchange):
+    @staticmethod
+    def get():
+        return {"status": "UP"}, 200
+
+class ShowBalance(Resource):
+    @staticmethod
+    def get(exchange):
         abort_if_invalid_exchange(str(exchange).lower())
         args = balance_get_args.parse_args()
 
@@ -46,11 +52,10 @@ class Balance(Resource):
                 "balance": balance}, 200
 
 class ShowTransactions(Resource):
-    def get(self, exchange, pair):
+    @staticmethod
+    def get(exchange, pair):
         abort_if_invalid_exchange(str(exchange).lower())
         args = transastions_get_args.parse_args()
-
-
 
         if exchange == "gemini":
             transactions = gemini.show_transactions(pair)
@@ -63,12 +68,26 @@ class ShowTransactions(Resource):
                 return valid_transactions
         elif exchange == "kraken":
             transactions = kraken.show_transactions(pair)
+            if args["since"] is not None:
+                valid_transactions = []
+                from datetime import datetime as dt
+                for transaction in transactions:
+                    if transaction["time"] > dt.strptime(args["since"], "%d-%m-%Y").timestamp():
+                        valid_transactions.append(transaction)
+                return valid_transactions
         else:
             transactions = bitstamp.show_transactions(pair)
-
+            if args["since"] is not None:
+                valid_transactions = []
+                from datetime import datetime as dt
+                for transaction in transactions:
+                    if dt.strptime(transaction["datetime"], "%Y-%m-%d %H:%M:%S.%f") > dt.strptime(args["since"], "%d-%m-%Y"):
+                        valid_transactions.append(transaction)
+                return valid_transactions
         return transactions, 200
 
-api.add_resource(Balance, '/balance/<exchange>')
+api.add_resource(HealthCheck, "/healthcheck")
+api.add_resource(ShowBalance, '/balance/<exchange>')
 api.add_resource(ShowTransactions, '/transactions/<exchange>/<pair>')
 
 
